1521dc08e0183bfc9fd6a649af06c610a936fe08
dcz <857999639@qq.com>
2017-05-22 14:43:30 -0700

    c

--- app/src/main/java/com/example/duan/duan/DCZ_application/MyApplication.java
+++ app/src/main/java/com/example/duan/duan/DCZ_application/MyApplication.java
@@ -1,6 +1,7 @@
 package com.example.duan.duan.DCZ_application;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -9,9 +10,14 @@ import com.facebook.drawee.backends.pipeline.Fresco;
  */
 
 public class MyApplication extends Application{
+    private static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
+        context=getApplicationContext();
         Fresco.initialize(this);
+    }
+    public static Context getContext(){
+        return context;
     }
 }
